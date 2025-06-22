c719576490007330661778d3e80f1124ef565c00
PonDevCode <pondevcode@gmail.com>
2025-06-22 19:45:12 +0700

    text

--- config/db.js
+++ config/db.js
@@ -1,18 +1,17 @@
-const mongoose = require('mongoose') // import thư viện mongoose
-mongoose.set('strictQuery',true); // chỉ cho phép các thuộc tính được xác định trong schema của mô hình được sử dụng trong các truy vấn. 
-require('dotenv').config();
+    const mongoose = require('mongoose') // import thư viện mongoose
+    mongoose.set('strictQuery',true); // chỉ cho phép các thuộc tính được xác định trong schema của mô hình được sử dụng trong các truy vấn. 
+    require('dotenv').config();
 
-const URL = process.env.MONGO_DB_URL;
 
-const connect = async () =>{
-    try {
-        await mongoose.connect(URL,{})
-        console.log('kết nối thành công');
-    } catch (error) {
-        console.log('kết nối thất bại');
-        console.log(error);
+    const connect = async () =>{
+        try {
+            await mongoose.connect('mongodb+srv://pondevcode:vMPWO9hW3hVJ4y5Q@pondev.liucfuf.mongodb.net/BA?retryWrites=true&w=majority&appName=Pondev',{})
+            console.log('kết nối thành công');
+        } catch (error) {
+            console.log('kết nối thất bại');
+            console.log(error);
+        }
+
     }
-
-}
-    
-module.exports = {connect}
+        
+    module.exports = {connect}
